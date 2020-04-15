86fd1c835335e3d878acd4de898bebd45d81783c
Brian Wells <wells@users.noreply.github.com>
2020-04-15 11:49:23 -0500

    Create Meetings.php

---
+++ src/Api/Meetings.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace GeneralSystemsVehicle\Zoom\Api;
+
+use GuzzleHttp\Client;
+use Illuminate\Support\Arr;
+use GuzzleHttp\Exception\RequestException;
+use GeneralSystemsVehicle\Zoom\Guzzle\Api;
+
+class Meetings extends Api
+{
+    /**
+     * Get the meetings index.
+     *
+     * @param  string $userId
+     * @param  array  $payload
+     * @return array|null
+     */
+    public function index($userId = 'me', $payload = [])
+    {
+        if (! Arr::has($payload, 'authorization')) {
+            return null;
+        }
+
+        return $this->try(function() use ($userId, $payload)
+        {
+            return $this->client->get('v2/users/'.$userId.'/meetings', [
+                'query' => Arr::get($payload, 'query', []),
+                'headers' => [
+                    'Authorization' => Arr::get($payload, 'authorization'),
+                ],
+            ]);
+        });
+    }
+
+    /**
+     * Get a meeting.
+     * @param  string $meetingId
+     * @param  array  $payload
+     * @return array|null
+     */
+    public function get($meetingId, $payload = [])
+    {
+        if (! Arr::has($payload, 'authorization')) {
+            return null;
+        }
+
+        return $this->try(function() use ($meetingId, $payload)
+        {
+            return $this->client->get('v2/meetings/'.$meetingId, [
+                'query' => Arr::get($payload, 'query', []),
+                'headers' => [
+                    'Authorization' => Arr::get($payload, 'authorization'),
+                ],
+            ]);
+        });
+    }
+
+    /**
+     * Create a meeting.
+     * @param  string $userId
+     * @param  array  $payload
+     * @return array|null
+     */
+    public function create($userId = 'me', $payload = [])
+    {
+        if (! Arr::has($payload, 'authorization')) {
+            return null;
+        }
+
+        if (! Arr::has($payload, 'body')) {
+            return null;
+        }
+
+        return $this->try(function() use ($userId, $payload)
+        {
+            return $this->client->post('v2/users/'.$userId.'/meetings', [
+                'body' => Arr::get($payload, 'body'),
+                'headers' => [
+                    'Authorization' => Arr::get($payload, 'authorization'),
+                ],
+            ]);
+        });
+    }
+
+    /**
+     * Update a meeting
+     * @param  string $meetingId
+     * @param  array  $payload
+     * @return array|null
+     */
+    public function update($meetingId, $payload = [])
+    {
+        if (! Arr::has($payload, 'authorization')) {
+            return null;
+        }
+
+        if (! Arr::has($payload, 'body')) {
+            return null;
+        }
+
+        return $this->try(function() use ($meetingId, $payload)
+        {
+            return $this->client->patch('v2/meetings/'.$meetingId, [
+                'body' => Arr::get($payload, 'body'),
+                'query' => Arr::get($payload, 'query', []),
+                'headers' => [
+                    'Authorization' => Arr::get($payload, 'authorization'),
+                ],
+            ]);
+        });
+    }
+
+    /**
+     * Delete a meeting.
+     * @param  string $meetingId
+     * @param  array  $payload
+     * @return array|null
+     */
+    public function delete($meetingId, $payload = [])
+    {
+        if (! Arr::has($payload, 'authorization')) {
+            return null;
+        }
+
+        return $this->try(function() use ($meetingId, $payload)
+        {
+            return $this->client->delete('v2/meetings/'.$meetingId, [
+                'query' => Arr::get($payload, 'query', []),
+                'headers' => [
+                    'Authorization' => Arr::get($payload, 'authorization'),
+                ],
+            ]);
+        });
+    }
+}
