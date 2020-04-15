209d0a7fe6d1c59a7c8b4a7d2194d4486a47f529
Brian Wells <wells@users.noreply.github.com>
2020-04-15 11:49:10 -0500

    Create ZoomServiceProvider.php

---
+++ src/ZoomServiceProvider.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace GeneralSystemsVehicle\Zoom;
+
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\ServiceProvider;
+
+class ZoomServiceProvider extends ServiceProvider
+{
+    use EventMap;
+    use ServiceBindings;
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->bootEvents();
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerConfig();
+        $this->registerServices();
+    }
+
+    /**
+     * Register the package events.
+     *
+     * @return void
+     */
+    protected function bootEvents()
+    {
+        $dispatcher = $this->app->make(Dispatcher::class);
+
+        foreach ($this->events as $event => $listeners) {
+            foreach ($listeners as $listener) {
+                $dispatcher->listen($event, $listener);
+            }
+        }
+    }
+
+    /**
+     * Setup the configuration for the package.
+     *
+     * @return void
+     */
+    protected function registerConfig()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/config.php', 'zoom'
+        );
+
+        $this->publishes([
+            __DIR__.'/../config/config.php' => config_path('zoom.php'),
+        ], 'zoom');
+    }
+
+    /**
+     * Register package services in the container.
+     *
+     * @return void
+     */
+    protected function registerServices()
+    {
+        foreach ($this->serviceBindings as $key => $value) {
+            is_numeric($key)
+                    ? $this->app->singleton($value)
+                    : $this->app->singleton($key, $value);
+        }
+    }
+}
